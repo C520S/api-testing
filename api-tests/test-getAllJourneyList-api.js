a90d603c4b199437e0f1e009fffa647dece084b2
C520S <71176236+C520S@users.noreply.github.com>
2023-05-12 21:26:05 +0300

    Optimisation code

--- api-tests/test-getAllJourneyList-api.js
+++ api-tests/test-getAllJourneyList-api.js
@@ -1,34 +1,34 @@
-const chai = require('chai');
-const axios = require('axios');
-const Ajv = require('ajv');
+const chai = require("chai");
+const axios = require("axios");
+const Ajv = require("ajv");
 const ajv = new Ajv();
-const journeyListschemas = require('../schemas/jourenyLIstschemas.json')
+const journeyListschemas = require("../schemas/jourenyLIstschemas.json");
 
-describe('Get all journey list Api request', async()=>{
+describe("Get all journey list Api request", async () => {
+  it("should get the journey list", async () => {
+    const response = await axios.get(
+      "https://talented-visor-tick.cyclic.app/api/v1/journeys?page=1"
+    );
 
-    it('should get the journey list' , async()=>{
-        const response = await axios.get('https://talented-visor-tick.cyclic.app/api/v1/journeys?page=1')
+    chai.expect(response.status).equals(200);
+    chai.expect(response.data.status).equals("success");
+    chai.expect(response.data.results).equals(15);
+    chai.expect(response.data.data.totalPages).equal(51375);
+    const validate = ajv.compile(journeyListschemas);
+    const valid = validate(response.data);
+    chai.expect(valid).to.be.true;
+  });
 
-        chai.expect(response.status).equals(200)
-        chai.expect(response.data.status).equals("success")
-        chai.expect(response.data.results).equals(15)
-        chai.expect(response.data.data.totalPages).equal(51375)
-        const validate = ajv.compile(journeyListschemas);
-        const valid = validate(response.data);
-        chai.expect(valid).to.be.true;
-    })
-
-    it('should be searched for by the name Kaleva in the journey list' , async()=>{
-        const response = await axios.get('https://talented-visor-tick.cyclic.app/api/v1/journeys?search=Kaleva')
-        chai.expect(response.status).equals(200)
-        chai.expect(response.data.status).equals("success")
-        chai.expect(response.data.results).equals(15)
-        chai.expect(response.data.data.totalPages).equal(322)
-        const validate = ajv.compile(journeyListschemas);
-        const valid = validate(response.data);
-        chai.expect(valid).to.be.true;
-      
-    })
-
-  
-})
+  it("should be searched for by the name Kaleva in the journey list", async () => {
+    const response = await axios.get(
+      "https://talented-visor-tick.cyclic.app/api/v1/journeys?search=Kaleva"
+    );
+    chai.expect(response.status).equals(200);
+    chai.expect(response.data.status).equals("success");
+    chai.expect(response.data.results).equals(15);
+    chai.expect(response.data.data.totalPages).equal(322);
+    const validate = ajv.compile(journeyListschemas);
+    const valid = validate(response.data);
+    chai.expect(valid).to.be.true;
+  });
+});
--- api-tests/test-getSingleStationView.js
+++ api-tests/test-getSingleStationView.js
@@ -1,25 +1,22 @@
-const chai = require('chai');
-const axios = require('axios');
-const Ajv = require('ajv');
+const chai = require("chai");
+const axios = require("axios");
+const Ajv = require("ajv");
 const ajv = new Ajv();
 
-const singleStationschemas = require('../schemas/singleStationschemas.json')
+const singleStationschemas = require("../schemas/singleStationschemas.json");
 
-describe.only ('Get single station view Api request', async()=>{
+describe.only("Get single station view Api request", async () => {
+  it("should give you the data for the single station called Mankkaanlaaksontie", async () => {
+    const response = await axios.get(
+      "https://talented-visor-tick.cyclic.app/api/v1/stationList/Mankkaanlaaksontie"
+    );
 
-    it('should give you the data for the single station called Mankkaanlaaksontie' , async()=>{
-        const response = await axios.get('https://talented-visor-tick.cyclic.app/api/v1/stationList/Mankkaanlaaksontie')
+    chai.expect(response.status).equals(200);
+    chai.expect(response.data.status).equals("success");
 
-        chai.expect(response.status).equals(200)
-        chai.expect(response.data.status).equals("success")
- 
-        const validate = ajv.compile(singleStationschemas);
-        const valid = validate(response.data);
-       
-        chai.expect(valid).to.be.true;
-    })
+    const validate = ajv.compile(singleStationschemas);
+    const valid = validate(response.data);
 
-  
-
-  
-})
+    chai.expect(valid).to.be.true;
+  });
+});
